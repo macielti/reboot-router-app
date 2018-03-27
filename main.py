411eecf1b04a972e398b4c873144e901834b7352
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2018-03-27 02:06:23 -0300

    primeira versao

---
+++ main.py
@@ -0,0 +1,65 @@
+import requests
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class Tela(BoxLayout):
+    
+    def login(self):
+        cookies = {
+            'login': '`elho.`elho.r`wd',
+        }
+
+        headers = {
+            'Host': '192.168.1.1',
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3',
+            'Accept-Encoding': 'gzip, deflate',
+            'Referer': 'http://192.168.1.1/',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': '61',
+            'Connection': 'close',
+            'Upgrade-Insecure-Requests': '1',
+        }
+
+        data = 'loginUsername=admin&loginPassword=admin&rememberMe=rememberMe'
+
+        response = requests.post('http://192.168.1.1/goform/login', headers=headers, cookies=cookies, data=data, verify=False)
+        
+
+    def reboot(self):
+        
+        
+        self.login()
+    
+        cookies = {
+            'login': '`elho.`elho.r`wd',
+        }
+
+        headers = {
+            'Host': '192.168.1.1',
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3',
+            'Accept-Encoding': 'gzip, deflate',
+            'Referer': 'http://192.168.1.1/RgSetup.asp',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': '194',
+            'Connection': 'close',
+            'Upgrade-Insecure-Requests': '1',
+        }
+
+        data = 'LocalIpAddressIP0=192&LocalIpAddressIP1=168&LocalIpAddressIP2=1&LocalIpAddressIP3=1&WanLeaseAction=0&WanConnectionType=0&MtuSize=0&RestoreFactoryNo1=0x00&ApplyRgSetupAction=0&ApplyRebootAction=1'
+
+        response = requests.post('http://192.168.1.1/goform/RgSetup', headers=headers, cookies=cookies, data=data, verify=False)
+        
+        
+
+class RebootRouter(App):
+
+    def build(self):
+        return Tela()
+
+
+if __name__ == "__main__":
+    RebootRouter().run()
